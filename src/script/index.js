23c3723c778f79ca9716b269e6a9507aaba4a839
Aditya Tiwari <aditya.tiwari@joshtechnologygroup.com>
2025-06-15 23:30:23 +0530

    mobile version with navigation drawer

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,30 @@
 import '../styles/main.scss';
 
-console.log('hello from index.js');
+let hamburgerIcon = document.querySelector('.icon-hamburger');
+let navigationDrawer = document.querySelector('.navigation-drawer');
+let navigationDrawerNavlinksContainer = document.querySelector(
+    '.navigation-drawer__navlinks-container',
+);
+let closeNavigationDrawerButton = document.querySelector(
+    '.navigation-drawer__close-icon',
+);
+
+function closeNavigationDrawer(ele) {
+    ele.addEventListener('click', () => {
+        navigationDrawer.style.display = 'none';
+        hamburgerIcon.style.visibility = 'visible';
+    });
+}
+
+hamburgerIcon.addEventListener('click', () => {
+    hamburgerIcon.style.visibility = 'hidden';
+    navigationDrawer.style.display = 'flex';
+    navigationDrawer.style.flexDirection = 'column';
+});
+
+closeNavigationDrawer(navigationDrawer);
+closeNavigationDrawer(closeNavigationDrawerButton);
+
+navigationDrawerNavlinksContainer.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
