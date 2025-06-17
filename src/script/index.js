ab6b242d211e29835531006ed0c6973837dcc7f7
Aditya Tiwari <aditya.tiwari@joshtechnologygroup.com>
2025-06-17 13:34:43 +0530

    barrel files added

--- src/script/index.js
+++ src/script/index.js
@@ -8,13 +8,24 @@ let navigationDrawerCloseIcon = document.querySelector(
 );
 
 hamburgerIcon.addEventListener('click', () => {
-    navlinksContainer.style.display = 'flex';
-    navlinksContainer.style.position = 'absolute';
-    hamburgerIcon.style.display = 'none';
+    // navlinksContainer.style.display = 'flex';
+    // navlinksContainer.style.position = 'absolute';
+    // hamburgerIcon.style.display = 'none';
+
+    navlinksContainer.classList.add('navbar-container-hamburger--clicked');
+    navlinksContainer.classList.add('navbar-container--show');
+    hamburgerIcon.classList.add('element--hide');
 });
 
 navigationDrawerCloseIcon.addEventListener('click', () => {
-    hamburgerIcon.style.display = 'flex';
-    navlinksContainer.style.position = 'static';
-    navlinksContainer.style.display = 'none';
+    // hamburgerIcon.style.display = 'flex';
+    // navlinksContainer.style.position = 'static';
+    // navlinksContainer.classList.remove("navbar-container-hamburger--clicked");
+    // navlinksContainer.classList.add("element--hide");
+
+    // hamburgerIcon.classList.remove("element--hide");
+    // navlinksContainer.style.display = 'none';
+    navlinksContainer.classList.toggle('navlinks-container--hide');
+    // navlinksContainer.classList.add("navlinks-container--hide");
+    // hamburgerIcon.style.display = 'flex';
 });
