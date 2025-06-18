740b8082814b0e6481e8d6774376d7b6615e7b9d
Aditya Tiwari <aditya.tiwari@joshtechnologygroup.com>
2025-06-18 15:20:38 +0530

    complete working of header section (navbar + navigation menu)

--- src/script/index.js
+++ src/script/index.js
@@ -3,29 +3,18 @@ import '../styles/main.scss';
 let hamburgerIcon = document.querySelector('.icon-hamburger');
 
 let navlinksContainer = document.querySelector('.navlinks__container');
-let navigationDrawerCloseIcon = document.querySelector(
-    '.navlinks__container-close-icon',
-);
+
+let buttonContainer = document.querySelector('.navbar__button-container');
 
 hamburgerIcon.addEventListener('click', () => {
     // navlinksContainer.style.display = 'flex';
     // navlinksContainer.style.position = 'absolute';
+    // buttonContainer.style.display="flex";
+    navlinksContainer.classList.toggle(
+        'navbar__navigation-drawer-navlinks--active',
+    );
+    buttonContainer.classList.toggle(
+        'navbar__navigation-drawer-button-container--active',
+    );
     // hamburgerIcon.style.display = 'none';
-
-    navlinksContainer.classList.add('navbar-container-hamburger--clicked');
-    navlinksContainer.classList.add('navbar-container--show');
-    hamburgerIcon.classList.add('element--hide');
-});
-
-navigationDrawerCloseIcon.addEventListener('click', () => {
-    // hamburgerIcon.style.display = 'flex';
-    // navlinksContainer.style.position = 'static';
-    // navlinksContainer.classList.remove("navbar-container-hamburger--clicked");
-    // navlinksContainer.classList.add("element--hide");
-
-    // hamburgerIcon.classList.remove("element--hide");
-    // navlinksContainer.style.display = 'none';
-    navlinksContainer.classList.toggle('navlinks-container--hide');
-    // navlinksContainer.classList.add("navlinks-container--hide");
-    // hamburgerIcon.style.display = 'flex';
 });
