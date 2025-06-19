fc641552153b72b0300f0cede5cf84de708642d4
Aditya Tiwari <aditya.tiwari@joshtechnologygroup.com>
2025-06-19 11:35:43 +0530

    head detached latest commit

--- src/script/index.js
+++ src/script/index.js
@@ -1,20 +1,12 @@
-// import '../styles/main.scss';
-
 let hamburgerIcon = document.querySelector('.icon-hamburger');
-
 let navlinksContainer = document.querySelector('.navlinks__container');
-
 let buttonContainer = document.querySelector('.navbar__button-container');
 
 hamburgerIcon.addEventListener('click', () => {
-    // navlinksContainer.style.display = 'flex';
-    // navlinksContainer.style.position = 'absolute';
-    // buttonContainer.style.display="flex";
     navlinksContainer.classList.toggle(
         'navbar__navigation-drawer-navlinks--active',
     );
     buttonContainer.classList.toggle(
         'navbar__navigation-drawer-button-container--active',
     );
-    // hamburgerIcon.style.display = 'none';
 });
